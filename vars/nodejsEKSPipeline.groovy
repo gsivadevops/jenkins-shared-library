4341fc9de4aaa2ac37f336a6e833e1753be859ce
sivakumar <gsivadevops@gmail.com>
2025-10-04 22:14:12 +0530

    nodejs pipeline updated to trigger deploy

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -165,7 +165,7 @@ def call(Map configMap){
                 steps {
                     script {
                         //build job: 'catalogue-cd',
-                        build job: "../${COMPONENT}-cd"
+                        build job: "../${COMPONENT}-cd",
                         parameters: [
                             string(name: 'appVersion', value: "${appVersion}"),
                             string(name: 'deploy_to', value: 'dev')
